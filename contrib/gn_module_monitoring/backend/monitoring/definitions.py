1cc67a6a0f2a913f738bc44f57d65ed62d14251a
Antoine Cecchi <antoine.cecchi@mnhn.fr>
2021-10-08 11:30:11 +0200

    Add monitoring

---
+++ contrib/gn_module_monitoring/backend/monitoring/definitions.py
@@ -0,0 +1,43 @@
+from .models import (
+    TMonitoringModules,
+    TMonitoringSites,
+    TMonitoringVisits,
+    TMonitoringObservations,
+    TMonitoringObservationDetails,
+    TMonitoringSitesGroups,
+)
+from .objects import (
+    MonitoringModule,
+    MonitoringSite
+)
+
+from .base import monitoring_definitions
+from .repositories import MonitoringObject
+from .geom import MonitoringObjectGeom
+
+
+'''
+    MonitoringModels_dict :
+    Fait le lien entre les monitoring_objects
+    et les modèles sqlalchemy
+'''
+
+MonitoringModels_dict = {
+    'module': TMonitoringModules,
+    'site': TMonitoringSites,
+    'visit': TMonitoringVisits,
+    'observation': TMonitoringObservations,
+    'detail': TMonitoringObservationDetails,
+    'sites_group': TMonitoringSitesGroups
+}
+
+MonitoringObjects_dict = {
+    'module': MonitoringModule,  # besoin pour retrouver le module depuis module_code à voir si on peux faire sans
+    'site': MonitoringSite,
+    'visit': MonitoringObject,
+    'observation': MonitoringObject,
+    'observation_detail': MonitoringObject,
+    'sites_group': MonitoringObject,
+}
+
+monitoring_definitions.set(MonitoringObjects_dict, MonitoringModels_dict)
